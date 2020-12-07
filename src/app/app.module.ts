d7877f07ec1d78a37ffbe7e685beb80273ac7668
Adrian <adrianrojasanchez@gmail.com>
2020-12-07 11:15:13 -0600

    instalando componentes de bootstrap, accordion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app.routing';
 import { ViewsModule } from './views/views.module';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { LayoutModule } from './views/shared/layout/layout.module';
@@ -16,7 +17,8 @@ import { LayoutModule } from './views/shared/layout/layout.module';
     AppRoutingModule,
     ViewsModule,
     HttpClientModule,
-    LayoutModule
+    LayoutModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/views/auth/auth.module.ts
+++ src/app/views/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth.routing';
 import { FormsModule } from '@angular/forms';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
 
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
@@ -13,7 +14,8 @@ import { ForgotPasswordComponent } from './forgot-password/forgot-password.compo
     imports: [
         CommonModule,
         FormsModule,
-        AuthRoutingModule
+        AuthRoutingModule,
+        AccordionModule
     ]
 })
 export class AuthModule { }
